1d576e632fb86f0bde350e9c236d1d8acb073c00
kerhac <marius@myidea.fr>
2021-04-15 11:30:46 +0200

    update linter

--- App/Screens/LoginScreen.js
+++ App/Screens/LoginScreen.js
@@ -1,27 +1,31 @@
 import React from 'react'
 import {View, Text, Button} from 'react-native'
 import { GoogleSignin } from '@react-native-community/google-signin';
-import auth from '@react-native-firebase/auth';
+import auth from '@react-native-firebase/auth'; // 1
 
 GoogleSignin.configure({
-  webClientId: '954837257818-2nlon6a9javmmf6m9o4nnbufooufhdf0.apps.googleusercontent.com',
-  offlineAccess: true
+  webClientId: '954837257818-2nlon6a9javmmf6m9o4nnbufooufhdf0.apps.googleusercontent.com', // 2
+  offlineAccess: true // 3
 });
 
 
 const LoginScreen = () => {
   async function onGoogleButtonPress() {
-    try {
+    try { // 4
       // Get the users ID token
+      // On connecte l'utilisateur avec Google
       const { idToken } = await GoogleSignin.signIn();
-    
+
+      console.log(idToken)
+      
+      // On envoit l'information (le token Google) à notre backend, ici Firebase
       // Create a Google credential with the token
       const googleCredential = auth.GoogleAuthProvider.credential(idToken);
     
       // Sign-in the user with the credential
       return auth().signInWithCredential(googleCredential);
       
-    } catch (e) {
+    } catch (e) { // 4
       console.error(e)
     }
   }
