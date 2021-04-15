d60aee25775e1e8944b42738d8f0449a2945916f
kerhac <marius@myidea.fr>
2021-04-15 09:58:56 +0200

    fix google auth

--- App/Screens/LoginScreen.js
+++ App/Screens/LoginScreen.js
@@ -1,22 +1,29 @@
 import React from 'react'
 import {View, Text, Button} from 'react-native'
 import { GoogleSignin } from '@react-native-community/google-signin';
+import auth from '@react-native-firebase/auth';
 
 GoogleSignin.configure({
-  webClientId: '954837257818-la44iqtid0p6pg19tbaiq7dhj8kutsjc.apps.googleusercontent.com',
+  webClientId: '954837257818-2nlon6a9javmmf6m9o4nnbufooufhdf0.apps.googleusercontent.com',
+  offlineAccess: true
 });
 
 
 const LoginScreen = () => {
   async function onGoogleButtonPress() {
-    // Get the users ID token
-    const { idToken } = await GoogleSignin.signIn();
-  
-    // Create a Google credential with the token
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-  
-    // Sign-in the user with the credential
-    return auth().signInWithCredential(googleCredential);
+    try {
+      // Get the users ID token
+      const { idToken } = await GoogleSignin.signIn();
+    
+      // Create a Google credential with the token
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    
+      // Sign-in the user with the credential
+      return auth().signInWithCredential(googleCredential);
+      
+    } catch (e) {
+      console.error(e)
+    }
   }
 
   return (
